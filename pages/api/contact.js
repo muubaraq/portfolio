c0b38857eeafb9381d0ff46a74035e5b327ba1ca
mubaraq <paynedaze@gmail.com>
2024-02-23 00:29:05 +0100

    ch: contact

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,19 +1,19 @@
 const nodemailer = require('nodemailer')
 require('dotenv').config()
 
-export default function (req, res) {
+export default function sendMail (req, res) {
   const transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.zoho.com",
+    
+    service: "gmail",
     auth: {
-      user: "codegiyu@zohomail.com",
+      user: "sixthlogic@gmail.com",
       pass: process.env.password,
     },
-    secure: true,
+  
   })
   const mailData = {
-    from: "codegiyu@zohomail.com",
-    to: "codegiyu@gmail.com",
+    from: "sixthlogic@gmail.com",
+    to: "sixthlogic@gmail.com",
     subject: `Message From ${req.body.name}`,
     text: req.body.message + " | Sent from: " + req.body.email + " | Phone: " + req.body.phone,
     html: `<div>${req.body.message}</div><p>Sent from:
